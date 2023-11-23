e67834dafe8f8bba8238598103764196a3236d59
Teoliten <123169946+Teoliten@users.noreply.github.com>
2023-11-23 10:35:58 +0200

    formatting

--- YEAR 2/test1/main.cpp
+++ YEAR 2/test1/main.cpp
@@ -4,28 +4,31 @@
 
 using namespace std;
 
-void print(const vector<int> vctr) // print vector
+// Function to print the elements of a vector
+void print(const vector<int> vctr)
 {
   for (int i = 0; i < vctr.size(); i++)
   {
     cout << vctr[i] << " ";
   }
-  cout << endl
-       << endl;
-} // print
+  cout << endl << endl;
+}//print
 
+// Function to swap two elements in a vector
 void swapVal(vector<int> &vctr, int pos1, int pos2)
 {
   int tmp = vctr[pos1];
   vctr[pos1] = vctr[pos2];
   vctr[pos2] = tmp;
-} // swapVal
+}//swapVal
 
+// Function to partition the vector for quicksort
 int partition(vector<int> &vctr, int front, int end)
 {
   int pivot = vctr[front];
 
-  int count = 0; // amount of numbers greater then pivot element
+  int count = 0; // Number of elements greater than the pivot
+
   for (int i = (front + 1); i <= end; i++)
   {
     if (vctr[i] <= pivot)
@@ -34,12 +37,13 @@ int partition(vector<int> &vctr, int front, int end)
     }
   }
 
-  int index = front + count; // pos to have space for numbers > pivot on left, number < pivot on right
+  int index = front + count; // Position for the pivot
   swapVal(vctr, index, front);
 
   int l = front;
   int k = end;
 
+  // Partition the vector
   while (l < index && k > index)
   {
     while (vctr[l] <= pivot)
@@ -61,9 +65,10 @@ int partition(vector<int> &vctr, int front, int end)
   }
 
   return index;
-} // partition
+}//partition
 
-void quickSort(vector<int> &vctr, int front, int end) // quicksort vector
+// Function to perform quicksort on a vector
+void quickSort(vector<int> &vctr, int front, int end)
 {
   if (front >= end)
   {
@@ -72,20 +77,24 @@ void quickSort(vector<int> &vctr, int front, int end) // quicksort vector
 
   int part = partition(vctr, front, end);
 
+  // Recursively call quickSort on the partitions
   quickSort(vctr, front, part - 1);
   quickSort(vctr, part + 1, end);
-} // quickSort
+}//quickSort
 
+// Function to remove duplicates from a sorted vector
 void removeDuplicates(vector<int> &vctr)
 {
   vector<int> result;
   result.push_back(vctr[0]);
   int prev = vctr[0];
 
+  // Iterate through the vector to remove duplicates
   for (int i = 1; i < vctr.size(); i++)
   {
     if (vctr[i] == prev)
     {
+      // Duplicate found, do not add to the result vector
     }
     else
     {
@@ -103,7 +112,8 @@ int main()
 
   vector<int> vctr;
 
-  for (int i = 0; i < input.length(); i++) // read input into vector
+  // Read input into the vector, converting characters to integers
+  for (int i = 0; i < input.length(); i++)
   {
     if (i % 2)
     {
@@ -111,21 +121,22 @@ int main()
     }
     else
     {
-      vctr.push_back(input[i] - 48); // char to int
+      vctr.push_back(input[i] - 48); // Convert char to int
     }
   }
 
-  cout << endl
-       << "Given vector: ";
+  cout << endl << "Given Vector: ";
   print(vctr);
 
-  quickSort(vctr, 0, (vctr.size() - 1)); // (vector, front, back)
-  cout << "Sorted vector (using quickSort): ";
+  // Sort the vector using quicksort
+  quickSort(vctr, 0, (vctr.size() - 1));
+  cout << "Sorted Vector (using Quicksort): ";
   print(vctr);
 
+  // Remove duplicates from the sorted vector
   removeDuplicates(vctr);
   cout << "Duplicates removed from sorted vector: ";
   print(vctr);
 
   return 0;
-} // main
+}// main
