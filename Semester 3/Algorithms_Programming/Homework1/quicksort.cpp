be68d5a039108c1bb94edeb6e2fbdca8e5567d55
Teoliten <123169946+Teoliten@users.noreply.github.com>
2024-03-30 22:01:41 +0200

    final changes for Homework 1

--- Semester 3/Algorithms_Programming/Homework1/quicksort.cpp
+++ Semester 3/Algorithms_Programming/Homework1/quicksort.cpp
@@ -4,88 +4,90 @@ using namespace std;
 
 int makePartition(int arr[], int a, int b)
 {
-  int pivot = arr[a];
-  int n = 0;
+    int pivot = arr[a]; // choose the first element as the pivot
+    int n = 0; // counter for elements smaller or equal to the pivot
 
-  for (int i = a + 1; i <= b; i++)
-  {
-    if (arr[i] <= pivot)
+    
+    for (int i = a + 1; i <= b; i++)// count the number of elements smaller than or equal to the pivot
     {
-      n++;
-    }
-  }
-
-  int pivotIndex = a + n;
-  swap(arr[pivotIndex], arr[a]);
-
-  int i = a, j = b;
-
-  while (i < pivotIndex && j > pivotIndex)
-  {
-
-    while (arr[i] <= pivot)
-    {
-      i++;
+        if (arr[i] <= pivot)
+        {
+            n++;
+        }
     }
 
-    while (arr[j] > pivot)
+    int pivotIndex = a + n;
+    swap(arr[pivotIndex], arr[a]); // swap the pivot with its correct position
+
+    int i = a, j = b;
+
+    
+    while (i < pivotIndex && j > pivotIndex) // move the elements to their correct sides of the pivot
     {
-      j--;
+        
+        while (arr[i] <= pivot)// move the left pointer until an element greater than the pivot is found
+        {
+            i++;
+        }
+
+        while (arr[j] > pivot)// move the right pointer until an element smaller than or equal to the pivot is found
+        {
+            j--;
+        }
+        
+        if (i < pivotIndex && j > pivotIndex)// swap the elements at i and j if they are on opposite sides of the pivot
+        {
+            swap(arr[i++], arr[j--]);
+        }
     }
 
-    if (i < pivotIndex && j > pivotIndex)
-    {
-      swap(arr[i++], arr[j--]);
-    }
-  }
-
-  return pivotIndex;
+    return pivotIndex;
 } // makePartition
 
-void quickSort(int arr[], int a, int b)
+void quickSort(int arr[], int a, int b) // using tail recursion to perform quicksort
 {
-  if (a >= b)
-  {
-    return;
-  }
+    if (a >= b) // base case
+    {
+        return; 
+    }
 
-  int part = makePartition(arr, a, b);
+    int part = makePartition(arr, a, b);
 
-  quickSort(arr, a, part - 1);
-
-  quickSort(arr, part + 1, b);
+    // recursively sort the lower and upper halves
+    quickSort(arr, a, part - 1); // sort the lower half
+    quickSort(arr, part + 1, b); // sort the upper half
 } // quickSort
 
 void printArr(int arr[], int size)
 {
-  cout << "Array:" << endl;
-  for (int i = 0; i < size; i++)
-  {
-    cout << arr[i] << ", ";
-  }
-  cout << endl
-       << endl;
+    cout << "Array:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << ", ";
+    }
+    cout << endl << endl;
 } // printArr
 
 int main()
 {
-  int n;
-  cin >> n;
-  int arr[n];
-  for (int i = 0; i < n; i++)
-  {
-    cin >> arr[i];
-  }
+    int n;
+    cin >> n;
+    int arr[n];
 
-  int size = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 0; i < n; i++) // read array
+    {
+        cin >> arr[i];
+    }
 
-  cout << "Unsorted ";
-  printArr(arr, size);
+    int size = sizeof(arr) / sizeof(arr[0]); 
 
-  quickSort(arr, 0, size - 1);
+    cout << "Unsorted ";
+    printArr(arr, size); // print the unsorted array
 
-  cout << "Sorted ";
-  printArr(arr, size);
+    quickSort(arr, 0, size - 1); // sort the array using quicksort
 
-  return 0;
+    cout << "Sorted ";
+    printArr(arr, size); // print the sorted array
+
+    return 0;
 } // main
