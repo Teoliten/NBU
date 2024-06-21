af52b558049a7317f3a4badb1829f5ae42769f69
Teoliten <123169946+Teoliten@users.noreply.github.com>
2024-06-21 21:54:37 +0300

    exam3 start

--- Semester 4/Algorithms_Programming/Homework2/main.cpp
+++ Semester 4/Algorithms_Programming/Homework2/main.cpp
@@ -1,43 +1,35 @@
 #include <bits/stdc++.h>
 
-/*
-TASK:
-Provide an implementation of the breadth-first search algorithm on a graph
-represented with adjacency lists data structure. Provide that your implementation
-prints out the sate of the graph after each step of the algorithm, and shows how
-the color, d and \pi attributes of each vertex are changed. Represent the graph as a C++ class.
-Provide an iterative implementation of the function that prints a shortest path.
-*/
-
 using namespace std;
 
 class Graph
 {
 public:
-  Graph(int vert);                                            // constructor
-  void addEdgeDirected(int vertexA, int vertexB, int weight); // add edge between two vertices with a weight
-  void printGraph();                                          // print graph, showing connected vertices
-  void BFS(int start);                                        // Breadth-first search given a start vertex
+  Graph(int vert);                                    // constructor
+  void addEdge(int vertexA, int vertexB, int weight); // add edge between two vertices with a weight
+  void printGraph();                                  // print graph, showing connected vertices
+  void BFS(int start);                                // Breadth-first search given a start vertex
 
 private:
   int vertices;                          // number of verices in graph
   vector<pair<int, int>> *adjacencyList; // list containing all vertices, edges and weight
+  vector<int> state;                     // state of each vertex: 1-unvisited, 2-seen, 3-visited
 }; // Class Graph
 
 Graph::Graph(int vertices)
 {
   this->vertices = vertices;
   adjacencyList = new vector<pair<int, int>>[vertices];
+  state.resize(vertices, 1); // initialize all vertices as unvisited
 } // Constructor
 
-void Graph::addEdgeDirected(int vertexA, int vertexB, int weight)
+void Graph::addEdge(int vertexA, int vertexB, int weight)
 {
   adjacencyList[vertexA].push_back({vertexB, weight});
-} // addEdgeDirected
+} // addEdge
 
 void Graph::printGraph()
 {
-  cout << vertices;
   for (int i = 0; i < vertices; ++i)
   {
     cout << "\n Vertex " << i << ":";
@@ -52,7 +44,6 @@ void Graph::printGraph()
 void Graph::BFS(int start)
 {
   vector<int> distance(vertices, INT_MAX);
-  vector<bool> visited(vertices, false);
 
   priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
@@ -65,7 +56,14 @@ void Graph::BFS(int start)
     pq.pop();
 
     // Mark the vertex as visited
-    visited[u] = true;
+    state[u] = 3;
+
+    // Print the state of each vertex
+    for (int i = 0; i < vertices; ++i)
+    {
+      cout << "Vertex " << i << ": State: " << state[i] << ", Distance: " << (distance[i] == INT_MAX ? "not reached" : to_string(distance[i])) << endl;
+    }
+    cout << endl;
 
     // Traverse all adjacent vertices of u
     for (auto edge : adjacencyList[u])
@@ -74,37 +72,35 @@ void Graph::BFS(int start)
       int weight = edge.second;
 
       // Relax the edge if it improves the distance to v
-      if (!visited[v] && distance[u] + weight < distance[v])
+      if (state[v] == 1) // unvisited vertex
       {
+        state[v] = 2; // seen but not visited
         distance[v] = distance[u] + weight;
         pq.push({distance[v], v});
       }
     }
   }
 
-  // Print the shortest distances to each vertex
-  for (int i = 0; i < vertices; ++i)
-  {
-    cout << "Vertex " << i << ": " << distance[i] << endl;
-  }
 } // BFS
 
 int main()
 {
-  Graph D(5); // Create a DIRECTED adjacency list with 5 vertices NOT WEIGHTED
-  D.addEdgeDirected(0, 1, 5);
-  D.addEdgeDirected(1, 2, 3);
-  D.addEdgeDirected(1, 3, 6);
-  D.addEdgeDirected(1, 4, 2);
-  D.addEdgeDirected(2, 3, 7);
-  D.addEdgeDirected(2, 1, 1);
-  D.addEdgeDirected(3, 4, 4);
-  D.addEdgeDirected(4, 2, 9);
+  Graph G(5); 
+  G.addEdge(0, 1, 5); // vertex 0 connected to vertex 1, with weight 5
+  G.addEdge(1, 2, 3);
+  G.addEdge(1, 3, 6);
+  G.addEdge(2, 3, 7);
+  G.addEdge(2, 1, 1);
+  G.addEdge(3, 4, 4);
+  G.addEdge(3, 4, 2);
+  G.addEdge(4, 2, 9);
 
-  cout << "DIRECTED:\n";
-  D.printGraph(); // print
-  cout << "BFS starting from vertex 0:\n";
-  D.BFS(0);
+  G.printGraph();
+
+  cout << "\nBFS starting from vertex: ";
+  int input;
+  cin >> input;
+  G.BFS(input);
 
   return 0;
 } // main
